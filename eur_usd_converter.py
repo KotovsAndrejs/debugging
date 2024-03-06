59f627a3357b568a52f890eac56a770b86e4256a
KotovsAndrejs <125635760+KotovsAndrejs@users.noreply.github.com>
2024-03-06 12:52:13 +0200

    Update eur_usd_converter.py

--- eur_usd_converter.py
+++ eur_usd_converter.py
@@ -2,26 +2,29 @@
 # Mērķis: 
 # iespēja konvertēt USD uz EUR un otrādi pēc 1.08 kursa
 
-eur_to_usd_rate = 1.09
+eur_to_usd_rate = 1.08
 
 print("Choose command")
-while False:
+while True:
     print("=======")
     print("1 - convert EUR into USD")
     print("2 - convert USD into EUR")
     print("3 - exit")
     command = input("")
-
-    if command == 1:
-        usd = input("Enter USD:")
-        eur = usd / 1.09
+    if command == "1":
+        usd = int(input("Enter USD:"))
+        eur = usd / eur_to_usd_rate
         print(usd, "USD equals EUR", eur)
-    elif command == 2:
-        usd = input("Enter EUR:")
-        eur = usd * 1.09
-        print(usd, "USD equals EUR", eur)
+    elif command == "2":
+        eur = int(input("Enter EUR:"))
+        usd = eur * eur_to_usd_rate
+        print(eur, "EUR equals USD", usd)
     else:
         break
 
 # Kādas kļūdas izdevies atrast?
-# 
+# samainīt kursu
+# samainīt 1.08 ar mainīgo eur_to_usd_rate
+# 2 komandā samainīt mainīgo nosaukumus (usd to eur, eur to usd)
+#
+
