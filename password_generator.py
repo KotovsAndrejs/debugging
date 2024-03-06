064ffd9d04a4c08cd2e93c7e18dfdda1bbe1fdb1
KotovsAndrejs <125635760+KotovsAndrejs@users.noreply.github.com>
2024-03-06 12:52:38 +0200

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,16 +1,22 @@
 # Mērķis: 
 # uzģenerēt paroli no burtiem un cipariem noteiktā garumā
-
-password_length = input("Kāds būs garums?")
+import random
+import string 
+password_length = int(input("Kāds būs garums?"))
 
 password = ""
-for i in range(12):
-if i % 2 == 0:
-passwrd += str(random.randint(0, 9))
-else
-password += randm.choice(string.ascii_letters)
+for i in range(password_length):
+    if i % 2 == 0:
+        password += str(random.randint(0, 9))
+    else:
+        password += random.choice(string.ascii_letters)
 
 print("Jauna parole:", password)
 
 # Kādas kļūdas izdevies atrast?
-# 
+# importēt random un string
+# for ciklā range = password_length
+# password_length = int(input("Kāds būs garums?"))
+# novietot atstarpes
+# izlabot sinaksiskas kļūdes
+#
